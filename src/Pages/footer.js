5e59ac9672bc56202f8183dd1b814a6dbb8e4b1d
SubiMask <subiabimask123@gmail.com>
2024-01-26 13:49:28 +0530

    two

---
+++ src/Pages/footer.js
@@ -0,0 +1,11 @@
+.service{
+    display: grid;
+    grid-template-coumns:
+    repeat(3, 1fr);
+    gap :20px;
+
+}
+.service-item {
+    background-color: red;
+    padding: 10 px;
+}
---
+++ src/route.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import {usestate, useEffect} from "react"
+export default function Effect() {
+    const[data, setData] = useState(null);
+}
